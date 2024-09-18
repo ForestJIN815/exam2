22177109c3a0441e9b0a8cd557ad6ff77e8ac197
진효찬 <attattahugen@gmail.com>
2024-09-18 13:16:17 +0900

    Revert "getter/setter 추가"
    This reverts commit 01744d6b43350f26f1f217bca4e88016d2562608.

--- ch1/src/Account.java
+++ ch1/src/Account.java
@@ -2,29 +2,4 @@ public class Account {
     private String owner;
     private String accoountNo;
     private int balance;
-
-    public String getOwner() {
-        return owner;
-    }
-
-    public void setOwner(String owner) {
-        this.owner = owner;
-    }
-
-    public String getAccoountNo() {
-        return accoountNo;
-    }
-
-    public void setAccoountNo(String accoountNo) {
-        this.accoountNo = accoountNo;
-    }
-
-    public int getBalance() {
-        return balance;
-    }
-
-    public void setBalance(int balance) {
-        this.balance = balance;
-    }
-
 }
