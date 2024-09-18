36fe52565eb68273524a785800806b12cd361fbe
진효찬 <attattahugen@gmail.com>
2024-09-18 13:26:02 +0900

    getter 메소드 추가

--- ch1/src/Customer.java
+++ ch1/src/Customer.java
@@ -1,4 +1,12 @@
 public class Customer {
     private String id;
     private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
